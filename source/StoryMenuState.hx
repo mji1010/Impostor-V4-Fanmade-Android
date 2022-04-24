5636c1c78986c3fd1edbec3fe17dec29361710d1
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-25 00:24:47 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -275,7 +275,7 @@ class StoryMenuState extends MusicBeatState
 		changeDifficulty();
 
                 #if android
-	        addVirtualPad(FULL, A_B);
+	        addVirtualPad(FULL, A_B_X_Y);
                 #end
 
 		super.create();
