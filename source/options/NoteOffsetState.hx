5241edf9f9ae5957a0b5e40d6453bd88e8095a87
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-25 00:28:55 +0300

    Update NoteOffsetState.hx

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -294,7 +294,7 @@ class NoteOffsetState extends MusicBeatState
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end)
 			{
 				for (i in 0...ClientPrefs.comboOffset.length)
 				{
@@ -332,7 +332,7 @@ class NoteOffsetState extends MusicBeatState
 				updateNoteDelay();
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end)
 			{
 				holdTime = 0;
 				barPercent = 0;
