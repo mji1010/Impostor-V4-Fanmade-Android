58261279be3706a7ff08323a3e23b64898b15fe1
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-25 00:38:34 +0300

    Update NotesSubState.hx

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -97,6 +97,10 @@ class NotesSubState extends MusicBeatSubstate
 		add(hsbText);
 
 		changeSelection();
+
+                #if android
+		addVirtualPad(FULL, A_B_C);
+                #end
 	}
 
 	var changingNote:Bool = false;
